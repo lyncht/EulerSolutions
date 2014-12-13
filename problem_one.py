06d386bc98a852149bd51508c3a1a31ae6d2f421
Tom Lynch <TLynch.UK1@gmail.com>
2014-12-13 19:20:02 +0000

    Added problem 1

---
+++ problem_one.py
@@ -0,0 +1,8 @@
+#Solution to problem 1 of Project Euler
+sumOfAnswers = 0
+for i in range(0,1000):
+    if i%3==0:
+        sumOfAnswers += i
+    elif i%5==0:
+        sumOfAnswers += i
+print "Sum=", sumOfAnswers
